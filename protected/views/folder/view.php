935b7f7b9d0ae35a32b9dc50dd4807b4930e730b
britech <brian.angyang@gmail.com>
2014-06-22 17:15:33 +0800

    updated links and display of relevant information

--- protected/views/folder/view.php
+++ protected/views/folder/view.php
@@ -3,26 +3,27 @@
 /* @var $model Folder */
 
 $this->breadcrumbs=array(
+	'Publications'=>array('publication/index'),
 	'Folders'=>array('index'),
-	$model->key_folder_group,
+	$model->fld_group_name,
 );
 
 $this->menu=array(
-	array('label'=>'List Folder', 'url'=>array('index')),
-	array('label'=>'Create Folder', 'url'=>array('create')),
-	array('label'=>'Update Folder', 'url'=>array('update', 'id'=>$model->key_folder_group)),
-	array('label'=>'Delete Folder', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_folder_group),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Folder', 'url'=>array('admin')),
+	array('label'=>'Manage Folder Groups', 'url'=>array('index')),
+	array('label'=>'Update Folder Group', 'url'=>array('update', 'id'=>$model->key_folder_group)),
+	array('label'=>'Delete Folder Group', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_folder_group),'confirm'=>'Are you sure you want to delete this item?')),
 );
 ?>
 
-<h1>View Folder #<?php echo $model->key_folder_group; ?></h1>
+<h1>Folder Information</h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'key_folder_group',
 		'fld_group_name',
-		'fld_description',
+		array(
+			'name'=>'fld_description',
+			'value'=>nl2br($model->fld_description)
+		),
 	),
 )); ?>
