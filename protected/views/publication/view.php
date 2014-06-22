4a008d4eb84ecbb9053ea6c2c8baf7832c7888f0
britech <brian.angyang@gmail.com>
2014-06-22 19:10:10 +0800

    update links and transformed some values to human readable texts

--- protected/views/publication/view.php
+++ protected/views/publication/view.php
@@ -4,31 +4,43 @@
 
 $this->breadcrumbs=array(
 	'Publications'=>array('index'),
-	$model->key_pub,
+	'Publication Information',
+);
+
+$this->profileLink=array(
+		array('label'=>'Authors', 'url'=>array('publication/author')),
+		array('label'=>'Files', 'url'=>array('publication/files')),
+		array('label'=>'Keywords', 'url'=>array('publication/keyword')),
 );
 
 $this->menu=array(
-	array('label'=>'List Publication', 'url'=>array('index')),
-	array('label'=>'Create Publication', 'url'=>array('create')),
-	array('label'=>'Update Publication', 'url'=>array('update', 'id'=>$model->key_pub)),
-	array('label'=>'Delete Publication', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_pub),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Publication', 'url'=>array('admin')),
+		array('label'=>'Update Publication', 'url'=>array('update', 'id'=>$model->key_pub)),
+		array('label'=>'Delete Publication', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_pub),'confirm'=>'Are you sure you want to delete this item?')),
+		array('label'=>'Publication Directory', 'url'=>array('index')),
+		array('label'=>'Upload a Publication', 'url'=>array('create')),
+		array('label'=>'Search a Publication', 'url'=>array('search')),
 );
 ?>
 
-<h1>View Publication #<?php echo $model->key_pub; ?></h1>
+<h1>Publication Information</h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'key_pub',
 		'fld_pub_title',
 		'fld_txt_page',
 		'fld_no_page',
-		'fld_location',
-		'key_dept',
-		'fld_date_stored',
-		'fld_format_type',
-		'fld_is_visible',
+		array(
+			'name'=>'key_dept',
+			'value'=>$model->department->fld_name
+		),
+		array(
+			'name'=>'fld_date_stored',
+			'value'=>$model->assembleHumanReadableDate($model->fld_date_stored)
+		),
+		array(
+			'name'=>'fld_is_visible',
+			'value'=>$model->getVisibilityDescription($model->fld_is_visible)
+		),
 	),
 )); ?>
