61ef16de37184b7ebee2dd824fd61962ae42136c
britech <brian.angyang@gmail.com>
2014-06-22 12:28:22 +0800

    updated the links of the view page and added human-readable values

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -4,29 +4,52 @@
 
 $this->breadcrumbs=array(
 	'Users'=>array('index'),
-	$model->key_user,
+	$model->fld_name,
 );
 
-$this->menu=array(
-	array('label'=>'List User', 'url'=>array('index')),
-	array('label'=>'Create User', 'url'=>array('create')),
-	array('label'=>'Update User', 'url'=>array('update', 'id'=>$model->key_user)),
-	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_user),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage User', 'url'=>array('admin')),
-);
+$defaultLinks=array(array('label'=>'User Directory', 'url'=>array('index')),
+					array('label'=>'Register a User', 'url'=>array('create')),
+					array('label'=>'Manage Users', 'url'=>array('admin')));
+
+switch($model->fld_user_stat){
+	case 0:
+		$additionalLinks=array(array('label'=>'Activate Account', 'url'=>array('updateStatus', 'id'=>$model->key_user, 'stat'=>1)));
+		break;
+	
+	case 1:
+		$additionalLinks=array(array('label'=>'Update User Information', 'url'=>array('update', 'id'=>$model->key_user)),
+							   array('label'=>'Deactivate Account', 'url'=>array('updateStatus', 'id'=>$model->key_user, 'stat'=>1)),
+							   array('label'=>'Block Account', 'url'=>array('updateStatus', 'id'=>$model->key_user, 'stat'=>-1)));
+		break;
+		
+	default:
+		$additionalLinks=array(array('label'=>'Deactivate Account', 'url'=>array('updateStatus', 'id'=>$model->key_user, 'stat'=>1)),
+							   array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->key_user),'confirm'=>'Are you sure you want to delete this item?')));
+		
+}
+$this->menu=array_merge($defaultLinks, $additionalLinks);
 ?>
 
-<h1>View User #<?php echo $model->key_user; ?></h1>
+<h1><?php echo $model->fld_name; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
 		'key_user',
-		'fld_name',
-		'fld_username',
-		'fld_password',
 		'fld_email_address',
-		'fld_restrictions',
-		'fld_user_stat',
+		/* 'fld_name', */
+		'fld_username',
+		array(
+			'name'=>'fld_password',
+			'value'=>hash_hmac('sha1', $model->fld_password, $model->fld_password)
+		),
+		array(
+			'name'=>'fld_restrictions',
+			'value'=>is_null($model->fld_restrictions) ? "Not Set" : $model->getRestrictionDescription($model->fld_restrictions)
+		),
+		array(
+			'name'=>'fld_user_stat',
+			'value'=>$model->getStatusDescription($model->fld_user_stat)
+		),
 	),
 )); ?>
