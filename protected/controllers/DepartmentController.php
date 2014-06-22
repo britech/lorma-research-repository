5212dc2828a7daa6c45483989d5b6b1a59fe5ae9
britech <brian.angyang@gmail.com>
2014-06-22 16:24:05 +0800

    changed the default redirection of deletion to the index page

--- protected/controllers/DepartmentController.php
+++ protected/controllers/DepartmentController.php
@@ -113,7 +113,7 @@ class DepartmentController extends Controller
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('index'));
 	}
 
 	/**
