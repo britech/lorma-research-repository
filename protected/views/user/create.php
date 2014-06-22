68610c2907a4d14f6df1ef91de7fdcf02f0e5e85
britech <brian.angyang@gmail.com>
2014-06-22 13:32:29 +0800

    style changes

--- protected/views/user/create.php
+++ protected/views/user/create.php
@@ -9,8 +9,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List User', 'url'=>array('index')),
-	array('label'=>'Manage User', 'url'=>array('admin')),
+	array('label'=>'User Directory', 'url'=>array('index')),
+	array('label'=>'Manage Users', 'url'=>array('admin')),
 );
 ?>
 
