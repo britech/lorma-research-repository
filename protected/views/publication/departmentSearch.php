ddfc8f69e151ae76ec33ef004fd63883b7d63fe3
britech <brian.angyang@gmail.com>
2014-08-30 19:37:33 +0800

    view addition
    added view for loading publication under the selected department

---
+++ protected/views/publication/departmentSearch.php
@@ -0,0 +1,32 @@
+<?php
+$this->breadcrumbs=array(
+		'Publications'=>array('index'),
+		'Department Search'
+);
+$this->menu=array(
+		array('label'=>'Search a Publication', 'url'=>array('search')),
+);
+?>
+<h1>Listing research publications under the
+	<span style="text-decoration: underline;"><?php echo $name;?></span>
+</h1>
+<?php
+$this->widget('zii.widgets.grid.CGridView', array(
+	'dataProvider'=>$dataProvider,
+	'columns'=>array(
+		array(
+			'name'=>'Publication Title',
+			'value'=>'$data->fld_pub_title',
+			'sortable'=>false
+		),
+		array(
+			'name'=>'Department of Origin',
+			'value'=>'$data->department->fld_name',
+			'sortable'=>false
+		),
+		array(
+			'class'=>'CButtonColumn',
+			'template'=>'{view}',
+		),
+	),
+)); ?>
