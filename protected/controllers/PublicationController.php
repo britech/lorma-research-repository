2d45afc782066021f1d0b98f431987c7f974f66f
britech <brian.angyang@gmail.com>
2014-08-31 16:04:20 +0800

    removed asset files since it is auto-generated

--- protected/controllers/PublicationController.php
+++ protected/controllers/PublicationController.php
@@ -123,13 +123,9 @@ class PublicationController extends Controller
 	 * If deletion is successful, the browser will be redirected to the 'admin' page.
 	 * @param integer $id the ID of the model to be deleted
 	 */
-	public function actionDelete($id)
-	{
+	public function actionDelete($id){
 		$this->loadModel($id)->delete();
-
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+		$this->redirect(array('index'));
 	}
 
 	/**
